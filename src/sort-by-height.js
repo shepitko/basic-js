a690fd2c5ae0b9618a90ddc2245388d2ac2941fb
Yurii Shepitko <ya.shepitko@gmail.com>
2022-05-02 23:21:39 +0300

    feat: finally final

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const sorted = arr
+    .filter(item => item !== -1)
+    .sort((a, b) => a - b)
+    .reverse();
+
+  const result = [];
+
+  arr.forEach(item => {
+    if (item === -1) result.push(item);
+    else result.push(sorted.pop());
+  })
+
+  return result;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('\'arr\' parameter must be an instance of the Array!')
+
+  const dcdn = '--discard-next';
+  const dcdp = '--discard-prev';
+  const dbln = '--double-next';
+  const dblp = '--double-prev';
+
+  const result = [];
+  let add = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] !== dcdn && arr[i] !== dcdp && arr[i] !== dbln && arr[i] !== dblp) {
+      for (let j = 0; j < add; j++) {
+        result.push(arr[i]);
+      }
+      add = 1;
+    }
+
+    if (arr[i] === dcdp) {
+      result.pop();
+      result.push(arr[i]);
+    }
+
+    if (arr[i] === dblp) {
+      if (result.length > 0) result.push(result[result.length - 1]);
+      result.push(arr[i]);
+    }
+
+    if (arr[i] === dcdn) {
+      result.push(arr[i]);
+      i++;
+    }
+
+    if (arr[i] === dbln) {
+      add++;
+      result.push(arr[i]);
+    }
+  }
+
+  return result.filter(item => item !== dcdn && item !== dcdp && item !== dbln && item !== dblp);
 }
 
 module.exports = {
